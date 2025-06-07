92345d3847e86b30f67d9a6e64e988e3bed7d259
Thomas Schoutsen <schoutse@gmail.com>
2025-06-07 12:13:57 +0200

    feat: papi fetch account info demo

---
+++ src/lib/papi/client.ts
@@ -0,0 +1,32 @@
+// `dot` is the name we gave to `npx papi add`
+import { ksm } from '@polkadot-api/descriptors'
+import { createClient } from 'polkadot-api'
+import { getSmProvider } from 'polkadot-api/sm-provider'
+import { chainSpec } from 'polkadot-api/chains/polkadot'
+import { startFromWorker } from 'polkadot-api/smoldot/from-worker'
+
+// Using vite
+import SmWorker from 'polkadot-api/smoldot/worker?worker'
+import type { SS58String } from 'polkadot-api'
+
+const worker = new SmWorker()
+
+const smoldot = startFromWorker(worker)
+const chain = await smoldot.addChain({ chainSpec })
+
+// Connect to the polkadot relay chain.
+const client = createClient(getSmProvider(chain))
+
+// With the `client`, you can get information such as subscribing to the last
+// block to get the latest hash:
+client.finalizedBlock$.subscribe((finalizedBlock) =>
+  console.log(finalizedBlock.number, finalizedBlock.hash),
+)
+
+// To interact with the chain, you need to get the `TypedApi`, which includes
+// all the types for every call in that chain:
+const typedApi = client.getTypedApi(ksm)
+
+// get the value for an account
+export const getAccountInfo = (address: SS58String) =>
+  typedApi.query.System.Account.getValue(address)
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,39 +1,33 @@
+import { useQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
-import logo from '../logo.svg'
+import { getAccountInfo } from '@/lib/papi/client'
 
 export const Route = createFileRoute('/')({
   component: App,
 })
 
 function App() {
+  const { data } = useQuery({
+    queryKey: ['people'],
+    queryFn: () =>
+      // Jay
+      getAccountInfo('E5qFqe5g5iS4Byu4hyKLAavayXtCiuuuK4xzuLcnQWvtqrg'),
+  })
+
   return (
-    <div className="text-center">
-      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-        <img
-          src={logo}
-          className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
-          alt="logo"
-        />
-        <p>
-          Edit <code>src/routes/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://tanstack.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn TanStack
-        </a>
-      </header>
+    <div className="p-4">
+      <h1 className="text-2xl mb-4">Jay's Account Balance</h1>
+      <ul>
+        <li>
+          <b>Free Balance:</b> {data?.data.free}
+        </li>
+        <li>
+          <b>Reserved Balance:</b> {data?.data.reserved}
+        </li>
+        <li>
+          <b>Frozen Balance:</b> {data?.data.frozen}
+        </li>
+      </ul>
     </div>
   )
 }
